bd59b1412864101cefc0effc909eb02266363e11
Sean <srcummings85@gmail.com>
2020-02-24 16:53:57 -0500

    second commit

---
+++ Controllers/TripController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCLab.Models;
+
+namespace MVCLab.Controllers
+{
+    public class TripController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MileageForm()//add view here
+        {
+            return View();
+        }
+        public IActionResult MileageResult(Tripometer trips)// letters matter to match
+        {
+            return View(trips);
+        }
+    }
+}
---
+++ Models/Tripometer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCLab.Models
+{
+    public class Tripometer
+    {
+        public double MPG { get; set; }
+        public double Gallons { get; set; }
+        public double Trip { get; set; }
+        public double Range 
+        {
+            get {return MPG* Gallons; }
+        }
+    }
+}
